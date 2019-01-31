e08990e9b5c19d308e60b2dda6fa86a7c2a823f3
Tânia Esteves <ainatheidi@outlook.com>
2019-01-31 15:07:35 +0000

    Adicionei criação de noticias

--- iBanda/routes/noticias.js
+++ iBanda/routes/noticias.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var axios = require('axios')
+var moment = require('moment');
 
 router.get('/', function(req, res) {
     axios.get('http://localhost:3000/api/noticias')
@@ -12,15 +13,30 @@ router.get('/', function(req, res) {
 });
 
 router.get('/criar-noticia', function(req, res) {
-    // axios.get('http://localhost:3000/api/noticias/' + req.params.id)
-    //     .then(noticia => res.render('noticia', {noticia: noticia.data}))
-    //     .catch(erro => {
-    //         console.log('Erro na consulta da noticia: ' + erro)
-    //         res.render('error', {error: erro, message: "Meu erro..."})
-    //     })
-    res.render('createNew')
+    res.render('criarNoticia')
 });
 
+router.post('/', function(req, res) {
+    axios.get('http://localhost:3000/api/users/user/email/' + req.session.email, { headers: { "Authorization": 'Bearer ' + req.session.token } })
+    .then(user => {
+        var params = {
+            titulo: req.body.titulo, pretitulo: req.body.pretitulo,
+            descricao: req.body.descricao, infos: req.body.infos,
+            data: new Date(Date.now()).toLocaleString(), autor: user.data.name, visivel: false
+        }        
+        axios.post('http://localhost:3000/api/noticias', params, { headers: { "Authorization": 'Bearer ' + req.session.token } })
+            .then(()=> res.redirect('http://localhost:3000/'))
+            .catch(erro => {
+                console.log('Erro na inserção de uma noticia: ' + erro)
+                res.render('error', {error: erro, message: "Meu erro ins..."})
+            })
+        })
+    .catch(erro => {
+        console.log('Erro na consulta da noticia: ' + erro)
+        res.render('error', {error: erro, message: "Meu erro..."})
+    })
+    
+});
 
 router.get('/:id', function(req, res) {
     axios.get('http://localhost:3000/api/noticias/' + req.params.id)
@@ -50,13 +66,5 @@ router.get('/autor/:id', function(req,res){
         })
 });
 
-router.post('/', function(req, res) {
-    axios.post('http://localhost:3000/api/noticias', req.body)
-        .then(()=> res.redirect('http://localhost:3000/noticias'))
-        .catch(erro => {
-            console.log('Erro na inserção de uma noticia: ' + erro)
-            res.render('error', {error: erro, message: "Meu erro ins..."})
-        })
-});
 
 module.exports = router;
